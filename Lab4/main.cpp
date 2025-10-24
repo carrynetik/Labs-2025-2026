1e9858e18d12c49b109c36c9eb062ca2220e4494
carrynetik <128978693+carrynetik@users.noreply.github.com>
2025-10-24 04:36:10 +0200

    LAB 4 is FINISHED

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 
 int main() {
-    // Пункт 1 - массив размером 7 (4 + 3)
-    std::cout << "=== Пункт 1 ===" << std::endl;
+    // пункт 1
+    std::cout << "Пункт 1" << std::endl;
     const int N = 7;  // Размер массива = 4 + 3
     int arr[N];
-    
-    // Ввод массива
+    // вводим массив
     std::cout << "Введите " << N << " чисел для массива:" << std::endl;
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {  // i=0 цикл начинается с нуля; i<N цикл повторяется пока не дойдем до N; ++i после каждого выполнения добавляем единичку
         std::cin >> arr[i];
     }
-    
-    // Проверяем, все ли числа кратны 5
-    bool all_multiple_of_5 = true;
+    // проверяем, все ли числа кратны 5
+    bool kratno_5 = true;  // все ли числа делятся на 5
     for (int i = 0; i < N; ++i) {
-        if (arr[i] % 5 != 0) {
-            all_multiple_of_5 = false;
-            break;
+        if (arr[i] % 5 != 0) {  // если число не делится на 5 без остатка
+            kratno_5 = false;  // значит не все числа кратны 5
+            break; 
         }
     }
-    
-    // Если все числа кратны 5 - сортируем массив
-    if (all_multiple_of_5) {
-        // Сортировка пузырьком (как в лекции)
-        for (int i = 0; i < N - 1; ++i) {
-            for (int j = i + 1; j < N; ++j) {
-                if (arr[i] > arr[j]) {
-                    // Меняем местами
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
+    // если все числа кратны 5 - сортируем массив
+    if (kratno_5) {
+        for (int i = 0; i < N - 1; ++i) {  // проходим по всем элементам кроме последнего
+            for (int j = i + 1; j < N; ++j) {  
+                if (arr[i] > arr[j]) { 
+                    // меняем местами, но еще можно использовать std::swap(arr[i], arr[j]);
+                    int tmp = arr[i];  
+                    arr[i] = arr[j];   
+                    arr[j] = tmp;      
                 }
             }
         }
@@ -39,7 +35,7 @@ int main() {
         std::cout << "Не все числа кратны 5, массив не изменен:" << std::endl;
     }
     
-    // Вывод массива
+    // вводим массив
     for (int i = 0; i < N; ++i) {
         std::cout << arr[i] << " ";
     }
@@ -47,52 +43,45 @@ int main() {
     
     std::cout << std::endl;
     
-    // Пункт 2 - матрица 3x4
-    std::cout << "=== Пункт 2 ===" << std::endl;
-    const int ROWS = 3;
-    const int COLS = 4;
-    int matrix[ROWS][COLS];
+
+
+    // Пункт 2
+    std::cout << "Пункт 2" << std::endl;
+    const int stroki = 3;  
+    const int stolb = 4;   
+    int matrix[stroki][stolb];
     
     // Ввод матрицы
-    std::cout << "Введите матрицу " << ROWS << "x" << COLS << ":" << std::endl;
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS; ++j) {
+    std::cout << "Введите матрицу " << stroki << "x" << stolb << ":" << std::endl;
+    for (int i = 0; i < stroki; ++i) {     
+        for (int j = 0; j < stolb; ++j) {  
             std::cin >> matrix[i][j];
         }
     }
-    
     // Ищем столбец с наибольшим количеством отрицательных чисел
-    int max_negative_col = 0;
-    int max_negative_count = 0;
-    
-    for (int j = 0; j < COLS; ++j) {
-        int negative_count = 0;
+    int max_stolb_otr = 0;      // номер столбца с макс количеством отрицательных
+    int max_kolvo_otr = 0;      // максимальное количество отрицательных в столбце
+    for (int j = 0; j < stolb; ++j) {  
+        int kolvo_otr = 0;  // счетчик отрицательных чисел в текущем столбце
         // Считаем отрицательные числа в столбце j
-        for (int i = 0; i < ROWS; ++i) {
-            if (matrix[i][j] < 0) {
-                negative_count++;
+        for (int i = 0; i < stroki; ++i) {  // перебираем все строки в столбце j
+            if (matrix[i][j] < 0) {  // если число отрицательное увеличиваем счетчик
+                kolvo_otr++;
             }
         }
-        
-        // Если нашли столбец с большим количеством отрицательных
-        if (negative_count > max_negative_count) {
-            max_negative_count = negative_count;
-            max_negative_col = j;
+        if (kolvo_otr > max_kolvo_otr) {
+            max_kolvo_otr = kolvo_otr;  // запоминаем новое максимальное количество
+            max_stolb_otr = j;          // запоминаем номер этого столбца
         }
     }
-    
-    std::cout << "Столбец с наибольшим количеством отрицательных: " << max_negative_col << std::endl;
-    std::cout << "Количество отрицательных в нем: " << max_negative_count << std::endl;
-    
-    // Заменяем все значения в найденном столбце на -1
-    for (int i = 0; i < ROWS; ++i) {
-        matrix[i][max_negative_col] = -1;
+    std::cout << "Столбец с наибольшим количеством отрицательных: " << max_stolb_otr << std::endl;
+    std::cout << "Количество отрицательных в нем: " << max_kolvo_otr << std::endl;
+    for (int i = 0; i < stroki; ++i) {
+        matrix[i][max_stolb_otr] = -1;  // заменяем каждое число в столбце на -1
     }
-    
-    // Вывод измененной матрицы
     std::cout << "Измененная матрица:" << std::endl;
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS; ++j) {
+    for (int i = 0; i < stroki; ++i) {
+        for (int j = 0; j < stolb; ++j) {
             std::cout << matrix[i][j] << " ";
         }
         std::cout << std::endl;
